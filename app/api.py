80f26d57058f1d0000a85a036ae433f71eb6eede
MauroAndretta <mauo.andretta222@gmail.com>
2022-11-16 18:37:13 +0100

    Updated the api, for loading the image with the prediction

--- app/api.py
+++ app/api.py
@@ -1,22 +1,26 @@
+import io
 import os
-
 import pickle
+import sys
 from datetime import datetime
 from functools import wraps
 from http import HTTPStatus
 from pathlib import Path
-import sys
+from tkinter import Image
 from typing import Dict, List
 
 import mlflow.keras
 import numpy as np
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, File, Request, UploadFile
 from keras.models import load_model
+from PIL import Image
 from sklearn.metrics import accuracy_score, confusion_matrix
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 dir = os.path.dirname(__file__)
 sys.path.insert(1, os.path.join(dir, "..", "src", "models"))
+
 from predict_model import create_test_set
 
 checkpoint_filepath_glasses = os.path.join(dir, "..", "models", "CNN")
@@ -106,4 +110,60 @@ def _predict(request: Request):
             "message": "Model not found",
             "status-code": HTTPStatus.BAD_REQUEST,
         }
+    return response
+
+
+# @app.post("/uploadfile/", tags=["Upload"])
+# @construct_response
+# async def create_upload_file(file: UploadFile = File(...)):
+    
+#     if not file:
+#         response = {
+#             "message": "No upload file sent",
+#             "status-code": HTTPStatus.BAD_REQUEST,
+#         }
+#     else:
+#         response = {
+#             "message": str(file.filename),
+#         }
+    
+#     return response
+
+@app.post("/uploadfile/")
+@construct_response
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
+
+@app.post('/prediction/')
+async def prediction_route(file:UploadFile = File(...)):
+
+    contents = await file.read()
+    pil_image = Image.open(io.BytesIO(contents))
+    pil_image = pil_image.resize((227, 227))
+
+    pil_image = pil_image.convert('RGB')
+   
+    numpy_image = np.array(pil_image) / 255.0
+    #numpy_image = np.expand_dims(numpy_image, axis=0)
+
+    # numpy_image.shape
+
+    img1 = []
+    img1.append(numpy_image)
+    img1 = np.array(img1)
+
+    prediction =best_model_glasses.predict(img1)
+    prediction = prediction.round()
+
+    if prediction[0] == 1:
+        response = {
+            "message": "You are wearing glasses!",
+            "status-code": HTTPStatus.OK,
+        }
+    else:
+        response = {
+            "message": "You are not wearing glasses!",
+            "status-code": HTTPStatus.OK,
+        }
+
     return response
