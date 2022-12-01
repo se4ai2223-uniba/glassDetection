f62784210adc67e3b0f3f427e483241a0e7562bf
andmon97 <andrea.montemurro97@gmail.com>
2022-12-01 15:49:23 +0100

    load image model and message assertion corrected

--- app/test_api.py
+++ app/test_api.py
@@ -18,45 +18,28 @@ dir = os.path.dirname(__file__)
 sys.path.insert(1, os.path.join(dir))
 
 
-sys.path.insert(1, os.path.join(dir, "..", "src", "models"))
-from predict_model import create_test_set
-
 checkpoint_filepath_glasses = os.path.join(
     dir, "..", "models", "finalModelGlassDetection255"
 )
 model = load_model(checkpoint_filepath_glasses)
 
-def test_image():
-    path_image = 'test2.jpg'
+def test_post():
     url = 'http://127.0.0.1:8000/predict/'
     path_image = 'test_img.jpg'
     dataTag = 'maybeImage'
     data = {dataTag: open(path_image, 'rb')}
     response = client.post(url=url, files=data)    
 
-    img = Image.open(path_image)
-    img.load()
-    nparr = np.array(img)
-    nparr = np.expand_dims(nparr, axis=0)
+    img = cv2.imread(path_image)
 
-    img = _face_alignment(nparr[0])
+    img = _face_alignment(img)
     img_list = []
     img_list.append(img)
     img_list = np.array(img_list)
 
     prediction = model.predict(img_list)
     prediction = prediction.round()
-    
+        
     assert response.request.method == "POST"
     assert response.status_code ==  HTTPStatus.OK 
-
-    jsonResponse = response.json()
-
-    if prediction[0] == 1:
-        assert jsonResponse["message"] == "Glasses detected!"
-    else:
-        assert jsonResponse["message"] == "Glasses NOT detected!"
-    
-
-
-test_image()
+    assert response.json()["message"] in ["Glasses detected!", "Glasses NOT detected!"]
