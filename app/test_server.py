d8ccc0bf6b2608a247347bf3c39eff4a826a0571
Gaetano Dibenedetto <tanodibe@hotmail.com>
2022-12-11 12:43:15 +0100

    action to test if the server is online

---
+++ app/test_server.py
@@ -0,0 +1,26 @@
+"""
+    Scripts for testing the api
+"""
+# pylint: disable=protected-access
+# pylint: disable=redefined-builtin
+# pylint: disable=import-error
+# pylint: disable=no-member
+# pylint: disable=wrong-import-order
+
+import requests
+from http import HTTPStatus
+
+
+def test_server():
+
+    """
+    Function for testing the api
+    """
+
+    url = "https://yfvpqbuhav.eu-west-1.awsapprunner.com/"
+    response = requests.get(url=url)
+    print(1)
+    print(response)
+
+    assert response.request.method == "GET"
+    assert response.status_code == HTTPStatus.OK
