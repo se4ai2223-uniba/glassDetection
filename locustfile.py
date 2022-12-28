71013f5646288872e02176e1c10f98a7e020d9fc
MauroAndretta <mauo.andretta222@gmail.com>
2022-12-28 11:17:51 +0100

    modified locustfile.py for pylint test

--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,28 @@
-import time
-from locust import HttpUser, task, between
-import os
-import cv2
-import numpy as np
+"""Script for load testing the api
+"""
 import io
+import os
+from locust import HttpUser, task, between
 from PIL import Image
 
 class TypicalIrisUser(HttpUser):
+    """Class to model an user
+
+    Args:
+        HttpUser (string): the object user created
+    """
     wait_time = between(1, 5)
 
     @task
     def sanity_check(self):
+        """function to check the availability of the api
+        """
         self.client.get("/")
 
     @task(5)
-    def CNN_prediction(self):
+    def cnn_prediction(self):
+        """Function to send a post request to the api
+        """
 
         #url = "https://yfvpqbuhav.eu-west-1.awsapprunner.com"
 
