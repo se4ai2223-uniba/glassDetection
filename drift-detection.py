5d0a1293157492110a75c36732dfb2547cda3b7d
MauroAndretta <mauo.andretta222@gmail.com>
2022-12-29 16:23:34 +0100

    modified drif-detection.py dor pylint and modified filter

--- drift-detection.py
+++ drift-detection.py
@@ -1,45 +1,67 @@
+"""
+Scripts for drift detection
+"""
+# pylint: disable=protected-access
+# pylint: disable=redefined-builtin
+# pylint: disable=import-error
+# pylint: disable=no-member
+# pylint: disable=wrong-import-order
+
 import os
 import sys
-
 import numpy as np
-import pytest
+#--------- IMPORT FOR DATA DRIFT --------
+from functools import partial
+
+from alibi_detect.cd import MMDDrift
+from alibi_detect.cd.tensorflow import preprocess_drift
+from timeit import default_timer as timer
 from keras.models import load_model
-from sklearn.metrics import accuracy_score
 
-dir = os.path.dirname(__file__)
-sys.path.insert(1, os.path.join(dir, "tests"))
+from imagecorruptions import corrupt
 
-from test_model import create_noise_sets
 
 dir = os.path.dirname(__file__)
 sys.path.insert(1, os.path.join(dir, "src", "models"))
 
-from predict_model import compute_model_accuracy, compute_predictions, create_test_set
-from train_model import create_train_val_sets, model_creation, model_training
+from predict_model import create_test_set
+from train_model import create_train_val_sets
 
-sys.path.insert(1, os.path.join(dir, "src", "data"))
-from make_dataset import (
-    _blur_pass,
-    _brightness_shift_pass,
-    _horizontal_flip_pass,
-    _noise_pass,
-)
+#function for the creation of a noise sets
+def create_noise_sets(train, val, test):
+    """Function used to create train, valid and test sets with some noise
 
-#--------- IMPORT FOR DATA DRIFT --------
-from functools import partial
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
+    Args:
+        train (array): original train set
+        val (array): original val set
+        test (array): original test set
 
-from alibi_detect.cd import MMDDrift
-from alibi_detect.models.tensorflow import scale_by_instance
-from alibi_detect.utils.fetching import fetch_tf_model
-from alibi_detect.saving import save_detector, load_detector
-from alibi_detect.datasets import fetch_cifar10c, corruption_types_cifar10c
-from alibi_detect.cd.tensorflow import preprocess_drift
+    Returns:
+        array: the noise versions of the sets
+    """
+    train_noise = []
+    val_noise = []
+    test_noise = []
 
+    for _, img in enumerate(train):
+        gaussian_noise_imgs = corrupt(img, corruption_number=7, severity=5)
+        train_noise.append(gaussian_noise_imgs)
 
-from timeit import default_timer as timer
+    for _, img in enumerate(val):
+        gaussian_noise_imgs = gaussian_noise_imgs = corrupt(img, corruption_number=7, severity=5)
+        val_noise.append(gaussian_noise_imgs)
+
+    for _, img in enumerate(test):
+        gaussian_noise_imgs = gaussian_noise_imgs = corrupt(img, corruption_number=6, severity=5)
+        test_noise.append(gaussian_noise_imgs)
+
+    train_noise = np.array(train_noise)
+    val_noise = np.array(val_noise)
+    test_noise = np.array(test_noise)
+
+    return train_noise, val_noise, test_noise
+
+#----------------- IMPLEMENTING DRIFT -------------------------------------------------
 
 # Create train and validation set
 X_train, y_train, X_valid, y_valid = create_train_val_sets()
@@ -61,18 +83,25 @@ glasses_model = load_model(CHECKPOINT_FILEPATH_GLASSES)
 preprocess_fn = partial(preprocess_drift, model=glasses_model, batch_size=32)
 
 # initialise drift detector
-cd = MMDDrift(X_test, backend='tensorflow', p_val=.05,
+cd = MMDDrift(X_train, backend='tensorflow', p_val=.05,
               preprocess_fn=preprocess_fn, n_permutations=100)
-
 
 labels = ['No!', 'Yes!']
 
-corruption = ['gaussian_noise']
+corruption = ['motion_blur']
 
 def make_predictions(cd, x_h0, x_corr, corruption):
-    t = timer()
+    """Function for detecting the drift
+
+    Args:
+        cd : the drift detector
+        x_h0 (list): original data
+        x_corr (list): corrupted data
+        corruption (list): the type of corruption
+    """
+    time_0 = timer()
     preds = cd.predict(x_h0)
-    dt = timer() - t
+    dt = timer() - time_0
     print('No corruption')
     print('Drift? {}'.format(labels[preds['data']['is_drift']]))
     print(f'p-value: {preds["data"]["p_val"]:.3f}')
@@ -80,9 +109,9 @@ def make_predictions(cd, x_h0, x_corr, corruption):
 
     if isinstance(x_corr, list):
         for x, c in zip(x_corr, corruption):
-            t = timer()
+            time_0 = timer()
             preds = cd.predict(x)
-            dt = timer() - t
+            dt = timer() - time_0
             print('')
             print(f'Corruption type: {c}')
             print('Drift? {}'.format(labels[preds['data']['is_drift']]))
