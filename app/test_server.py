5da681c722a8e8644fb1a6b6a7428e47788e29ac
Gaetano Dibenedetto <tanodibe@hotmail.com>
2022-12-30 18:23:35 +0100

    tests secrets

--- app/test_server.py
+++ app/test_server.py
@@ -17,7 +17,8 @@ def test_server():
     """
     Function for testing the api
     """
-    url = "ec2-34-246-171-145.eu-west-1.compute.amazonaws.com:8000/"
+    HOST_URL = os.environ.get("SSH_HOST")
+    url = "http://" + HOST_URL + ":8000/"
     response = requests.get(url=url)
 
     assert response.request.method == "GET"
